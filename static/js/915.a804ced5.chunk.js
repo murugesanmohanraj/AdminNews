5787f0fc86ca30d5b0f05d9db777de3512dfb1e6
Mohanraj M <mohanraj1711999@gmail.com>
2022-02-09 18:00:50 +0530

    Updates

---
+++ static/js/915.a804ced5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_coreui_coreui_free_react_admin_template=self.webpackChunk_coreui_coreui_free_react_admin_template||[]).push([[915],{1915:function(e,t,c){c.r(t);c(2791);var r=c(184);t.default=()=>(0,r.jsx)("div",{style:{flex:1,justifyContent:"center"},children:(0,r.jsx)("h1",{style:{textAlign:"center"},children:"Welcome to News Admin"})})}}]);
+//# sourceMappingURL=915.a804ced5.chunk.js.map

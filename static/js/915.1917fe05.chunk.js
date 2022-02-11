fe0035868a3e2bf44ee9a0f4674c02720464f389
mohanrajmurugasen <mohanraj1711999@gmail.com>
2022-02-11 20:38:27 +0530

    Updates

---
+++ static/js/915.1917fe05.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_coreui_coreui_free_react_admin_template=self.webpackChunk_coreui_coreui_free_react_admin_template||[]).push([[915],{61915:function(e,t,c){c.r(t);c(72791);var r=c(80184);t.default=()=>(0,r.jsx)("div",{style:{flex:1,justifyContent:"center"},children:(0,r.jsx)("h1",{style:{textAlign:"center"},children:"Welcome to News Admin"})})}}]);
+//# sourceMappingURL=915.1917fe05.chunk.js.map

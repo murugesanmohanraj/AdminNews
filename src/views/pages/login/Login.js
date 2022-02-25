c1a9122a5c5aaa33309e3f630de6f73c475d0d9c
Mohanraj M <81688486+mohanrajmurugasen@users.noreply.github.com>
2022-02-25 22:14:35 +0530

    Loging page updated

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -121,8 +121,7 @@ const Login = () => {
                   <div>
                     <h2>Sign up</h2>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
+                      Thank you for visiting our website of The Indian Express Group. We respect your privacy and consider it an important element of our business.
                     </p>
                     {/* <Link to="/register">
                       <CButton color="primary" className="mt-3" active tabIndex={-1}>
